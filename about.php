bd6ae5d138a4ef4eddc752d88910d1853eec4d8a
oadyse <rainmaker8621@gmail.com>
2022-05-19 20:49:54 +0700

    My first push

---
+++ about.php
@@ -0,0 +1,40 @@
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>About</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="assets/img/pic.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content">
+                <h3>Informatics Engineering Student (Final Year)</h3>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>8 Juni 2001</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 85784378582</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Magetan, East Java</span></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>sarieka.nm@gmail.com</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <p>
+                    Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                    Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End About Section -->
